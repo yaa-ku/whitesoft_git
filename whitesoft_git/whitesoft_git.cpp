cbb72a347a87dfe478ded3a681314397e3f9fb14
yanakuz <yana.yana.kuznecova666@gmail.com>
2022-05-02 16:10:35 +1000

    refactored code

--- whitesoft_git/whitesoft_git.cpp
+++ whitesoft_git/whitesoft_git.cpp
@@ -4,14 +4,16 @@
 
 using namespace std;
 
-void MyArray(array<float, 10>& arr)
-{
+void MyArray(array<float, 10>& arr) {
+
     int max = INT_MIN;
     float sum = 0;
-    float m = 0;
+    float arithmetic_average = 0;
+
     cout << "Array: ";
-    for (int i = 0; i < arr.size(); i++) {
+    for (size_t i = 0; i < arr.size(); i++) {
         arr[i] = -50 + rand() % 100;
+
         if (max < arr[i]) {
             max = arr[i];
         }
@@ -19,40 +21,44 @@ void MyArray(array<float, 10>& arr)
         sum += arr[i];
         cout << arr[i] << " ";
     }
+
     cout << endl << "Sum: " << sum << endl;
-    m = sum / arr.size();
-    cout << "Arithmetic average: " << m << endl;
+    arithmetic_average = sum / arr.size();
+    cout << "Arithmetic average: " << arithmetic_average << endl;
     cout << "max: " << max << ", max/2: " << 0.5 * max << endl;
 
     cout << "New array: ";
-    for (int i = 0; i < arr.size(); i++) {
+    for (size_t i = 0; i < arr.size(); i++) {
         cout << arr[i] / (0.5 * max) << " ";
     }
 }
 
-void MyVect(vector <float>& v) {
+void MyVect(vector <float>& vec) {
     int max = INT_MIN;
-    const int size = 10;
+    const size_t size = 10;
     float sum = 0;
-    float m = 0;
+    float arithmetic_average = 0;
 
     cout << "Vector: ";
-    for (int i = 0; i < size; i++) {
-        v.push_back(-50 + rand() % 100);
-        if (max < v[i])
-            max = v[i];
-        sum += v[i];
+    for (size_t i = 0; i < size; i++) {
+        vec.push_back(-50 + rand() % 100);
 
-        cout << v[i] << " ";
+        if (max < vec[i]) {
+            max = vec[i];
+        }
+
+        sum += vec[i];
+        cout << vec[i] << " ";
     }
+
     cout << endl << "Sum: " << sum << endl;
-    m = sum / v.size();
-    cout << "Arithmetic average: " << m << endl;
+    arithmetic_average = sum / vec.size();
+    cout << "Arithmetic average: " << arithmetic_average << endl;
     cout << "max: " << max << ", max/2: " << 0.5 * max << endl;
 
     cout << "New vector: ";
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i] / (0.5 * max) << " ";
+    for (size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] / (0.5 * max) << " ";
     }
 }
 
@@ -60,27 +66,28 @@ void MyList(list <float>& l)
 {
     int max = INT_MIN;
     list <float>::iterator it;
-    const int size = 10;
+    const size_t size = 10;
     float sum = 0;
-    float m = 0;
+    float arithmetic_average = 0;
 
     cout << "List: ";
-    for (int i = 0; i < size; i++)
-    {
+    for (size_t i = 0; i < size; i++) {
         int t = -50 + rand() % 100;
         l.push_back(t);
-        if (max < t)
+
+        if (max < t) {
             max = t;
+        }
     }
 
-    for (it = l.begin(); it != l.end(); it++)
-    {
+    for (it = l.begin(); it != l.end(); it++) {
         sum += *it;
         cout << *it << " ";
     }
+
     cout << endl << "Sum: " << sum << endl;
-    m = sum / l.size();
-    cout << "Arithmetic average: " << m << endl;
+    arithmetic_average = sum / l.size();
+    cout << "Arithmetic average: " << arithmetic_average << endl;
     cout << "max: " << max << ", max/2: " << 0.5 * max << endl;
 
     cout << "New list: ";
@@ -92,26 +99,28 @@ void MyList(list <float>& l)
 void MyDeque(deque<float>& d)
 {
     int max = INT_MIN;
-    const int size = 10;
+    const size_t size = 10;
     float sum = 0;
-    float m = 0;
+    float arithmetic_average = 0;
 
     cout << "Deque: ";
-    for (int i = 0; i < size; i++)
-    {
+    for (size_t i = 0; i < size; i++) {
         d.push_back(-50 + rand() % 100);
-        if (max < d[i])
+
+        if (max < d[i]) {
             max = d[i];
+        }
+
         sum += d[i];
         cout << d[i] << " ";
     }
     cout << endl << "Sum: " << sum << endl;
-    m = sum / d.size();
-    cout << "Arithmetic average: " << m << endl;
+    arithmetic_average = sum / d.size();
+    cout << "Arithmetic average: " << arithmetic_average << endl;
     cout << "max: " << max << ", max/2: " << 0.5 * max << endl;
 
     cout << "New deque: ";
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
     {
         cout << d[i] / (0.5 * max) << " ";
     }
